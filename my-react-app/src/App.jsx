9007dd37720f16a6ff7fa28a88074c200ad391ad
Rajesh97266 <rajesh97266@gmail.com>
2024-12-10 22:45:47 +0530

    second

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,25 @@
-
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route, } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
 
 function App() {
- 
-
   return (
     <>
-      <h1>My React App</h1>
-      <a href="/about.html">About</a>
-      <a href="/contact.html">Contact</a>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">Rajesh Website</div>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contacts</li>
+      </ul>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Navbar
