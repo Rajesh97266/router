b54901cfbe9829e5f54e08a8b1c1e70c2f1d0be3
Rajesh97266 <rajesh97266@gmail.com>
2024-12-10 21:21:19 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>My React App</h1>
+      <a href="/about.html">About</a>
+      <a href="/contact.html">Contact</a>
+    </>
+  );
+}
+
+export default App
